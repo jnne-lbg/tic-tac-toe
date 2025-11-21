e649713c99a652cfd24260558f97140eac6d7d57
jeanne <jeannelabuga@gmail.com>
2025-11-20 19:06:34 -0800

    Initialization of Scoreboard Text and Event Handlers for Each Cell

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let activePlayer = 1;
 let winMessage = document.getElementById("win-message");
 let scoreboardX = document.getElementById("scoreboard-x");
 let scoreboardO = document.getElementById("scoreboard-o");
+
+scoreboardX.innerText = sessionStorage.getItem("XWins") || 0;
+scoreboardO.innerText = sessionStorage.getItem("OWins") || 0;
+
+for (let i = 0; i < 9; i++) {
+   let cell = document.getElementById(`square-${i}`);
+   cell.addEventListener("click", handleCellClick);
+}
