4f59b3d93618b026fbfda36e84f65300e59cf44c
jeanne <jeannelabuga@gmail.com>
2025-11-20 19:07:52 -0800

    Added Function to Handle Cell Click

--- script.js
+++ script.js
@@ -31,3 +31,47 @@ function restartBoard() {
  
  }
  
+ function handleCellClick(event) {
+    let cellIndex = event.target.id[event.target.id.length - 1];
+    console.log(cellIndex)
+ 
+    if (activePlayer == 1) {
+        event.target.innerText = "X";
+        gameBoard[cellIndex] = 1;
+        activePlayer = 2;
+        event.target.removeEventListener("click", handleCellClick);
+ 
+        if (checkTie()) {
+            winMessage.innerText = "Tie";
+        }
+        if (checkWin(1)) {
+            winMessage.innerText = "Player 1 Has Won";
+            removeAllEventListeners();
+       
+            let value = Number(sessionStorage.getItem("XWins")) || 0;
+            value += 1;
+            sessionStorage.setItem("XWins", value);
+            scoreboardX.innerText = sessionStorage.getItem("XWins") || 0;
+        }
+   
+    } else if (activePlayer == 2) {
+        event.target.innerText = "O";
+        gameBoard[cellIndex] = 2;
+        activePlayer = 1;
+        event.target.removeEventListener("click", handleCellClick);
+ 
+        if (checkTie()) {
+            winMessage.innerText = "Tie";
+        }
+        if (checkWin(2)) {
+            winMessage.innerText = "Player 2 Has Won";
+            removeAllEventListeners();
+       
+            let value2 = Number(sessionStorage.getItem("OWins")) || 0;
+            value2 += 1;
+            sessionStorage.setItem("OWins", value2);
+            scoreboardO.innerText = sessionStorage.getItem("OWins") || 0;
+        }
+    }
+ }
+ 
