c48515232baa4d6ab577e8e6cc6ae0db3ae575b6
jeanne <jeannelabuga@gmail.com>
2025-11-20 18:59:38 -0800

    Added all Possible Winning Combinations

--- script.js
+++ script.js
@@ -78,7 +78,16 @@ function handleCellClick(event) {
 }
 
 function checkWin(player) {
-    if (gameBoard[0] == player && gameBoard[1] == player && gameBoard[2] == player) {
+    if 
+    ((gameBoard[0] == player && gameBoard[1] == player && gameBoard[2] == player) ||
+    (gameBoard[3] == player && gameBoard[4] == player && gameBoard[5] == player) ||
+    (gameBoard[6] == player && gameBoard[7] == player && gameBoard[8] == player) ||
+    (gameBoard[0] == player && gameBoard[3] == player && gameBoard[6] == player) ||
+    (gameBoard[1] == player && gameBoard[4] == player && gameBoard[7] == player) ||
+    (gameBoard[2] == player && gameBoard[5] == player && gameBoard[8] == player) ||
+    (gameBoard[0] == player && gameBoard[4] == player && gameBoard[8] == player) ||
+    (gameBoard[2] == player && gameBoard[4] == player && gameBoard[6] == player)
+    ) {
         return true;
     } else {
         return false;
