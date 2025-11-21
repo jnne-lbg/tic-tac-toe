417582951ef02e8a5bdd6f217c36be0b72f1ea31
jeanne <jeannelabuga@gmail.com>
2025-11-20 18:24:58 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// Todo: display message congratulating winner
+// add message if the game ends in a tie
+// be able to intializeBoard and states to restart
+// keep track of wins and losses
+
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let activePlayer = 1;
+let winMessage = document.getElementById("win-message");
+
+for (let i = 0; i < 9; i++) {
+    let cell = document.getElementById(`square-${i}`);
+    cell.addEventListener("click", handleCellClick);
+}
+
+function restartBoard() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    activePlayer = 1;
+
+    for (let i = 0; i < 9; i++) {
+        let cell = document.getElementById(`square-${i}`);
+        cell.innerText = '';
+    }
+
+    for (let i = 0; i < 9; i++) {
+        let cell = document.getElementById(`square-${i}`);
+        cell.addEventListener("click", handleCellClick);
+    }
+
+    winMessage.innerText = '';
+
+}
+
+function handleCellClick(event) {
+    let cellIndex = event.target.id[event.target.id.length - 1];
+    console.log(cellIndex)
+
+    if (activePlayer == 1) {
+        event.target.innerText = "X";
+        gameBoard[cellIndex] = 1;
+        activePlayer = 2;
+        event.target.removeEventListener("click", handleCellClick);
+
+        if (checkTie()) {
+            winMessage.innerText = "Tie";
+        }
+        if (checkWin(1)) {
+            winMessage.innerText = "Player 1 Has Won";
+        }
+    
+    } else if (activePlayer == 2) {
+        event.target.innerText = "O";
+        gameBoard[cellIndex] = 2;
+        activePlayer = 1;
+        event.target.removeEventListener("click", handleCellClick);
+        if (checkTie()) {
+            winMessage.innerText = "Tie";
+        }
+        if (checkWin(2)) {
+            winMessage.innerText = "Player 2 Has Won";
+        }
+    }
+}
+
+function checkWin(player) {
+    if (gameBoard[0] == player && gameBoard[1] == player && gameBoard[2] == player) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkTie() {
+    if (typeof gameBoard[0] === "number" &&
+        typeof gameBoard[1] === "number" &&
+        typeof gameBoard[2] === "number" &&
+        typeof gameBoard[3] === "number" &&
+        typeof gameBoard[4] === "number" &&
+        typeof gameBoard[5] === "number" &&
+        typeof gameBoard[6] === "number" &&
+        typeof gameBoard[7] === "number" &&
+        typeof gameBoard[8] === "number"
+    ) {
+        return true;
+    } return false;
+}
+
+let playAgainButton = document.getElementById("button-play-again");
+playAgainButton.addEventListener("click", restartBoard);
